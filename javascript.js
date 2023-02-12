304e4cadfe64c434773dc3f41d08a23a236dfc0d
AbdEl-Rahman21 <abdel292004@gmail.com>
2023-02-12 23:05:31 +0200

    add coloring and color selection feature

--- javascript.js
+++ javascript.js
@@ -1,9 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-  createSketch();
-});
-
 const grid = document.querySelector("input");
 grid.addEventListener("click", createSketch);
+grid.addEventListener("input", () => {
+  document.getElementById(
+    "sliderValue"
+  ).textContent = `${grid.value}X${grid.value}`;
+});
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    selectMode(button.id);
+  });
+});
 
 function createSketch() {
   let i = 0;
@@ -29,4 +37,43 @@ function createSketch() {
       outerPixel[i].appendChild(innerPixel);
     }
   }
+
+  selectMode("black");
 }
+
+function getRandomColor() {
+  const color = [0, 0, 0];
+
+  color[0] = Math.floor(Math.random() * 255);
+  color[1] = Math.floor(Math.random() * 255);
+  color[2] = Math.floor(Math.random() * 255);
+
+  return `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+}
+
+function selectMode(mode) {
+  const pixel = document.querySelectorAll(".innerPixel");
+
+  if (mode === "black") {
+    pixel.forEach((div) => {
+      div.addEventListener("mouseover", () => {
+        div.style.cssText = "background-color: black;";
+      });
+    });
+  } else if (mode === "rainbow") {
+    pixel.forEach((div) => {
+      div.addEventListener("mouseover", () => {
+        div.style.cssText = `background-color: ${getRandomColor()};`;
+      });
+    });
+  } else {
+    createSketch();
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById(
+    "sliderValue"
+  ).textContent = `${grid.value}X${grid.value}`;
+  createSketch();
+});
